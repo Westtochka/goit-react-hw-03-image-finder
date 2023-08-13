70058cbe85a0c83c40b8b869cb9b56acb9a56ca8
Olga Boiko <Itnumber1@ukr.net>
2023-08-13 23:58:55 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,79 +16,92 @@ class App extends Component{
     totalHits: 0,
     loading: false,
     showModal: false,
-    status: 'idle'
+    // status: 'idle'
   }
 
-  handlerSubmit = e => {
-    e.preventDefault();
-    const {text} = this.state;
-    if(text.trim()===''){
-      alert("Enter new text");     
-      return 
-    }
-    this.setState({text:'',
-    // hits: [], page: 1, totalHits: 0
-  })
-  }
+  // handlerSubmit = e => {
+  //   e.preventDefault();
+  //   const {text} = this.state;
+  //   if(text.trim()===''){
+  //     alert("Enter new text");   
+  // console.log('hh')  
+  //     return 
+  //   }
+  //   this.setState({text:'' }
+    
+  // )
+  // console.log('hh')
+  // }
 
 
-handleFormSubmit=text=>{console.log(this.state)
-this.setState({text})}
+handleFormSubmit=text=>{
+this.setState({
+  text,
+  hits: [],
+  page: 1,
+  totalHits: 0,
+})
+}
 
 handleImageClick = (imageURL) => {
   this.setState({ showModal: true, largeImageURL: imageURL }
     );
+    console.log(this.state.showModal)
+    console.log(this.state.totalHits)
 };
 handleModalClick = () => {
   this.setState({ showModal: false, largeImageURL: '' });
+  console.log(this.state.showModal)
+  console.log(this.state.totalHits)
 };
 buttonLoadClick = () => {
-  this.setState(prevState => ({ page: prevState.page + 1 }));
+  this.setState((prevState) => ({
+    page: prevState.page + 1,
+    }));
 };
 
 
 componentDidUpdate(prevProps, prevState){
-  const { text,   } = this.state;
-  if(text !== prevState.text){ 
-  console.log('Изменился текст инпута!')
-  this.setState({ loading: true });
-  this.setState({status: 'pending'})
-fetch(`https://pixabay.com/api/?q=${text}&page=1&image_type=photo&orientation=horizontal&per_page=12&key=36749422-339c82364b645e4ed63871096`)
-.then(response=>{
-  if (response.ok) {
-    return response.json();
-  }
-  return Promise.reject(new Error('нет такого изображения'));
-})
+  const { text, page  } = this.state;
+  if(text !== prevState.text || prevState.page !== page){ 
+    this.fetchData();
+  }}
 
-.then(data => {
-  const hitsArray = data.hits;
-  if (hitsArray.length !== 0) {
-    this.setState(prevState => ({
-      hits: [...prevState.hits, ...hitsArray],
-      status: 'resolved',
-      loading: false
-    }));
-  } else {
-    this.setState({ loading: false });
-    alert('I am sorry...There are no images for you');
-  }
-})
-// this.setState(prevState => ({
-//   hits: [...prevState.hits, ...this.state.hits],
-//   // totalHits: data.totalHits,
-//   loading: false,
-// })
-// )
 
-.catch(error => {
-  console.error(error);
-  this.setState({ 
-    status: 'rejected', 
-    loading: false });
-});
-}
-}
+  fetchData = () => {
+    const { text, page } = this.state;
+  
+    this.setState({ loading: true });
+  
+    fetch(`https://pixabay.com/api/?q=${text}&page=${page}&image_type=photo&orientation=horizontal&per_page=12&key=36749422-339c82364b645e4ed63871096`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(new Error('нет такого изображения'));
+      })
+      .then(data => {
+        const hitsArray = data.hits.map(hit => ({
+          ...hit,
+          id: hit.id + page, // Генерируем уникальный ключ для новых элементов hits
+        }));
+        console.log(`You have ${data.totalHits} images`)
+        if (hitsArray.length !== 0) {
+          this.setState(prevState => ({
+            hits: [...prevState.hits, ...hitsArray],
+            totalHits: data.totalHits,
+            loading: false
+          }));
+        } else {
+          this.setState({ loading: false });
+          alert('I am sorry...There are no images for you');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({ loading: false });
+      });
+  };
 
 render(){
   const { hits, loading, largeImageURL, showModal, totalHits}=this.state;
@@ -129,18 +142,35 @@ render(){
         )} 
         {/* <ToastContainer/> */}
         {loading && <Loader />}
-        {totalHits > 0 && hits.length < totalHits && (
+        {totalHits >= 0 && hits.length < totalHits && (
           <Button onBtnClick={this.buttonLoadClick} 
           />
         )} 
        {showModal && (
           <Modal onClose={this.handleModalClick}>
-            <img src={largeImageURL} alt="Modal" />
+            <img src={largeImageURL} alt="BigImage" />
           </Modal>
         )}
       </div>
        )
   
+
+////////////
+// openModal = (imageURL) => {
+//   this.setState({ isModalOpen: true, largeImageURL: imageURL });
+// };
+
+// handleModalClick = () => {
+//   this.setState({ showModal: false, largeImageURL: '' });
+//   console.log(this.state.showModal)
+//   console.log(this.state.totalHits)
+// };
+/////////////
+
+
+
+
+
  }
 
 }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,34 +1,3 @@
-// import { Component } from 'react'
-
-// export class ImageGalleryItem extends Component{
-// state={
-//       hits:[],
-    
-// };
-
-
-// render(){
-// return (
-//   <li >
-//     <h1>ImageGallery</h1>
-//     <p>{this.props.text}</p>
-//     <ul>
-//       {this.state.hits.map(hit => (
-//         <li key={hit.id}>
-//           <img src={hit.previewURL} alt={hit.tags} />
-//         </li>
-//       ))}
-//     </ul>
-//   </li>
-// );
-
-// }
-// }
-
-
-// import PropTypes from 'prop-types';
-
-
 export const ImageGalleryItem = ({ hits, onImage }) => {
   return (
     <>    
